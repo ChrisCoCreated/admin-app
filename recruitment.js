0bca22d3cb7c6bb931516a5429cb273ff23405a1
ChrisCoCreated <mrpowergage@gmail.com>
2026-09-09 12:17:14 +0100

    Remove temporary HR access and refresh recruitment assets

--- recruitment.js
+++ recruitment.js
@@ -1,7 +1,7 @@
 import { createAuthController } from "./auth-common.js";
 import { FRONTEND_CONFIG } from "./frontend-config.js";
 import { createDirectoryApi } from "./directory-api.js";
-import { canAccessPage, renderTopNavigation } from "./navigation.js?v=20260601";
+import { canAccessPage, renderTopNavigation } from "./navigation.js?v=20260909";
 import { getRecruitmentWhatsAppUrl, normalizeRecruitmentPhoneForActions } from "./recruitment-whatsapp.js?v=20260702";
 
 const searchInput = document.getElementById("searchInput");
